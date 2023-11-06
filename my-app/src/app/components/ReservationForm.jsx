2f1535ae9176eeed9bb58574629be91d108d26c7
ines.balt <ines.brasao@gmail.com>
2023-11-06 22:36:11 +0000

    reservation form completed

--- my-app/src/app/components/ReservationForm.jsx
+++ my-app/src/app/components/ReservationForm.jsx
@@ -1,79 +1,52 @@
 import { useRouter } from "next/router";
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/reservationForm.module.css'
 
 
 
-export default function ReservationForm({date, hour, type}) {
+export default function ReservationForm({id}) {
   const router = useRouter()
   const [formData, setFormData] = useState()
+  const [errorMessage, setErrorMessage] = useState()
   
-
-  const id = router.asPath.split("/")[2]
   const options = {
     method: 'POST',
     headers: {
         'Content-Type': "application/json"
     },
     body: JSON.stringify({
-        "id": id
+        "id": id,
+        "data": formData
     })
 
   }
-  
 
-  async function updateProfile() {
+  async function updateBooking() {
 
-    const res = await fetch(`/api/profile`, optionsArtist);
+    const res = await fetch(`/api/updateBooking`, options);
 
     if(res.status === 412){
-      setErrorMessage("É obrigatório escolher um nome.")
-
+      const body = await res.json();
+      setErrorMessage(body.message)
+    } else if (res.status === 200){
+      router.push(`/home`)
     }
-
-
-    if (res.status === 200) {
-      return true
-    }
-
-    return false
   }
 
 
   const handleChange = (event) => {
-    const { name, value, files } = event.target;
+    const { name, value} = event.target;
     setFormData(pForm => {
-      if (name === "path") {
-        return { ...pForm, [name]: files[0] }
-      }
-      return {...pForm, [name]: value}
+        return { ...pForm, [name]: value }
     });
   };
   
 
- 
-
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    const formDat = new FormData()
-    
-    formDat.append("path", formData.path)
-
-    
-    if(formData.path !== undefined) {
-      const res = await fetch(`/api/addProfilePhoto/${formData._id}`, {
-        method: 'POST',
-        body: formDat
-      })
-    }
-    
-    const updatedProfile = await updateProfile()
-
-    if(updatedProfile){
-      router.push(`/myprofile/${formData._id}`)
-    }
+    updateBooking()
   };
 
 
@@ -82,7 +55,6 @@ export default function ReservationForm({date, hour, type}) {
   return <div>
   <div className={styles.ProfileControlContainer}>
     
-
     <form  className={styles.form} onSubmit={handleSubmit} id="profileControl">
         
     <div className={styles.containerInputText} >
@@ -97,11 +69,9 @@ export default function ReservationForm({date, hour, type}) {
       <label htmlFor="phone" className={styles.labelInputText}>Telemóvel</label> 
       <input className={styles.inputText} type="text" id="phone" name="phone"  onChange={handleChange}/>
     </div>
-      
+      {errorMessage ? <p>{errorMessage}</p> : null}
       <button className={styles.button} type="submit" >Alterar</button>
     </form>
-  
-
   </div>
   </div>
 
