6d9c203bb0e83cf5c0e22c5e378e268e2dee4f45
e-paiva <evellynpaiva01@gmail.com>
2023-11-07 10:21:59 +0000

    css

--- my-app/src/pages/reserveConfirmation.js
+++ my-app/src/pages/reserveConfirmation.js
@@ -3,17 +3,17 @@ import NavBar from '../app/components/NavBar'
 import styles from '../styles/chek.module.css'
 
 export default function Confirmation () {
-    return (
-        <>
+    return (<>
+        <div className={styles.Confirmationcontainer}>
         <div className={styles.logoWrapper}><img className={styles.logo} src="/icons/3.png" /></div>
         <div className={styles.cp}> 
         <div className={styles.logo}>
-        <img src="icons/check.png" /> 
+        <img src="icons/check.png"  width={150} height={150}/> 
         </div>
         <p>sua reserva foi feita com sucesso!</p>
-        <p>você receberá em seu e-mail com </p> 
-        <p>todos os dados da reserva.</p>
-        </div>
+        <p>você receberá em seu e-mail com 
+        todos os dados da reserva.</p>
+        </div></div>
         <NavBar/>
         </>
     )
