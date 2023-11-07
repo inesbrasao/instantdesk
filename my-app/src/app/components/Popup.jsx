2288a2eafac9fc1ad5537d2fe40760fdf32c0d0a
ines.balt <ines.brasao@gmail.com>
2023-11-07 09:29:22 +0000

    popup

---
+++ my-app/src/app/components/Popup.jsx
@@ -0,0 +1,14 @@
+import styles from "../../styles/navbar.module.css";
+
+export default function Popup() {
+
+
+   return <>
+   <div><img src=""/></div>
+   <div className={styles.mbway}>
+   <p>Pagamento em Curso</p>
+   <p>-------------------------</p>
+   <p>Verifique a app MbWay para concluir o pagamento no prazo máximo de 4 minutos.</p>
+   </div>
+   </>
+}
