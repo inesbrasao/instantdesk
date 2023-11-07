597f71cf6bb1367cdeb50f34f536f3c0e13a366c
Luís Patrão <141627048+patraoruisu1@users.noreply.github.com>
2023-11-07 09:57:55 +0000

    oi

--- my-app/src/pages/infopage.js
+++ my-app/src/pages/infopage.js
@@ -7,17 +7,15 @@ export default function Page() {
     return (
         <div class="sqs-html-content">
             <div className={styles.indexMain}>
-                <div className={styles.logo}>
-                    <img src='/icons/logo.png' width={200} height={200} />
-                </div>
+            <div className={styles.logoWrapper}><img className={styles.logo} src="/icons/3.png" /></div>
                 <div>
                     <h2>Sobre Nós</h2>
-                    <p class="sqsrte-small preFade fadeIn">Bem-vindo ao nosso site, o destino definitivo para profissionais, empreendedores e empresas que buscam uma nova abordagem para o local de trabalho. Nós somos mais do que apenas um espaço de coworking; somos uma comunidade dinâmica, um centro de inovação e colaboração. Nossa missão é proporcionar um espaço de trabalho que inspire, conecte e capacite indivíduos e empresas a alcançar seu pleno potencial. Acreditamos que o ambiente de trabalho pode ser um catalisador para o sucesso, e estamos comprometidos em fornecer as ferramentas e a comunidade necessárias para que isso aconteça.
+                    <p class="sqsrte-small preFade fadeIn">Bem-vindo ao nosso site, o destino definitivo para profissionais, empreendedores e empresas que buscam uma nova abordagem para o local de trabalho. O objetivo é proporcionar um espaço de trabalho que inspire, conecte e capacite indivíduos e empresas a alcançar seu pleno potencial. Acreditamos que o ambiente de trabalho pode contribuir para o sucesso, e estamos comprometidos em fornecer as ferramentas necessárias para que isso aconteça.
                     </p></div>
                 <div>
                     <h2>Serviços</h2>
                     <p class="sqsrte-small preFade fadeIn">
-                        Oferecemos uma variedade de salas de coworking totalmente equipadas e espaços de escritório compartilhados. Quer sejas um freelancer em busca de um local tranquilo para trabalhar, uma startup em crescimento ou uma empresa estabelecida que busca flexibilidade, temos a solução certa para ti.
+                        Oferecemos uma variedade de salas de coworking totalmente equipadas. Quer seja um freelancer em busca de um local tranquilo para trabalhar, uma startup em crescimento ou uma empresa estabelecida que busca flexibilidade, temos a solução certa para ti.
                     </p></div>
                 <h2>Galéria</h2>
                 <div>
